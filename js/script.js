23352dfd6598ea9ceb587ead8e280465c8e076ac
ManuelBenini <manuelbenini1905@gmail.com>
2022-04-29 15:13:55 +0200

    stampati animali dinamicamente (array All)

---
+++ js/script.js
@@ -0,0 +1,144 @@
+const fontIcons = [
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	}
+];
+
+console.log(fontIcons.length);
+
+const animals = fontIcons.filter(element => (element.type == 'animal'))
+const vegetables = fontIcons.filter(element => (element.type == 'vegetable'))
+const users = fontIcons.filter(element => (element.type == 'user'))
+
+console.log('Animali', animals);
+console.log('Vegetali', vegetables);
+console.log('Utenti', users);
+
+outputStamp(fontIcons)
+
+function outputStamp(array) {
+  const row = document.querySelector('.row');
+
+  array.forEach(element => {
+    const {name, prefix, type, family, color} = element;
+    row.innerHTML +=  
+    `
+      <div class="col mb-5">
+        <div>
+          <i class="${family} ${prefix}${name} ${type}"></i>
+          <span id="name">${name}</span>
+        </div>
+      </div>
+    `
+  });
+  
+}
